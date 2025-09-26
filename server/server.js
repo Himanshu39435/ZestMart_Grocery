2e8b5edba6e4be663f98e1f79cfbca257aadc5dc
Himanshu Singh <hk39435@gmail.com>
2025-09-27 00:21:16 +0530

    FINAL FIX: Updated CORS to allow Vercel access

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
  app.get("/", (req, res) => {
    res.send("API is Working.");
